1d4f8f4a11ca0f64a57d06165766923447236be6
yokattaa <sontung130701@gmail.com>
2022-11-02 18:23:56 +0700

    first commit

---
+++ js/t_custom.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+  $(".baner-slide").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: "linear",
+  });
+});
